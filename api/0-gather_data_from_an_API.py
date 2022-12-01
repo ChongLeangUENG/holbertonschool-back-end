21ad5f2a4999329aa5ee983ba21bfa71770dd455
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-01 13:55:30 +0100

    Test gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-
+"""Returns info about his/her TODO list progress by giving employee ID"""
 from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
+    response1 = get('https://jsonplaceholder.typicode.com/todos/')
+    data1 = response1.json()
     completed = 0
     total = 0
     tasks = []
     response2 = get('https://jsonplaceholder.typicode.com/users/')
     data2 = response2.json()
 
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+    for idx in data2:
+        if idx.get("id") == int(argv[1]):
+            employee = idx.get("name")
 
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
+    for idx in data1:
+        if idx.get("userId") == int(argv[1]):
 
-            if i.get('completed') is True:
+            if idx.get("completed") is True:
                 completed += 1
-                tasks.append(i.get('title'))
+                tasks.append(idx.get("title"))
 
-    print("Employee {} is done with tasks({}/{}):".format(employee,
-                                                          completed, total))
+    print("Employee {} is done with task({}/{}):".format(employee,
+                                                         completed, total))
 
-    for i in tasks:
-        print("\t {}".format(i))
+    for idx in tasks:
+        print("\t {}".format(idx))
