f38f013d2889e2030ef57a8e9961a045da2cec49
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-01 13:58:51 +0100

    Test gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
     data2 = response2.json()
 
     for idx in data2:
-        if idx.get("id") == int(argv[1]):
-            employee = idx.get("name")
+        if idx.get('id') == int(argv[1]):
+            employee = idx.get('name')
 
     for idx in data1:
-        if idx.get("userId") == int(argv[1]):
+        if idx.get('userId') == int(argv[1]):
             total += 1
 
-            if idx.get("completed") is True:
+            if idx.get('completed') is True:
                 completed += 1
-                tasks.append(idx.get("title"))
+                tasks.append(idx.get('title'))
 
     print("Employee {} is done with task({}/{}):".format(employee,
                                                          completed, total))
